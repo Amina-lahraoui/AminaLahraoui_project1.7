8e9b53135493659aa9c510addbc68a6bd8133f3a
Amina-Lahraoui <a.lahraoui@aui.ma>
2026-03-05 20:48:26 +0000

    Add Launcher class for TCPServer TO SOLVE THE JAR ISSUE

---
+++ tcp_socket_server/src/main/java/ma/project/Launcher.java
@@ -0,0 +1,12 @@
+package ma.project;
+
+/**
+ * Launcher class for TCPServer
+ * This class is used as the main entry point in the executable JAR
+ * It delegates to TCPServer.main() which handles the JavaFX Application
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        TCPServer.main(args);
+    }
+}
