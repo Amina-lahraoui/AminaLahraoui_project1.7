34a37dba6d73bb0742eb6855a85f73e63ce312e6
Amina-Lahraoui <a.lahraoui@aui.ma>
2026-03-05 20:48:50 +0000

    Add Launcher class for TCPClient

---
+++ tcp_socket_client/src/main/java/ma/project/Launcher.java
@@ -0,0 +1,12 @@
+package ma.project;
+
+/**
+ * Launcher class for TCPClient
+ * This class is used as the main entry point in the executable JAR
+ * It delegates to TCPClient.main() which handles the JavaFX Application
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        TCPClient.main(args);
+    }
+}
